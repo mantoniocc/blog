28f13b20530469dcfed3c93d2a4e56f4f7be4704
Antonio Cortés <mcortes.it@outlook.com>
2015-07-29 10:02:59 -0300

    Schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150721201549) do
+ActiveRecord::Schema.define(version: 20150729130110) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -20,16 +20,6 @@ ActiveRecord::Schema.define(version: 20150721201549) do
     t.boolean  "status",     default: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
   end
 
 end
